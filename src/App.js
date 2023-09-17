677f804dc9bf46cb77212f70bd532f3477755f58
Shivesh_Pandey <86975173+Shiv16100@users.noreply.github.com>
2023-09-17 22:21:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Navbar from './components/navbar/Navbar.jsx';
 import './App.css';
+import Topview from './components/TopView/Topview.jsx';
+import HiringPartners from './components/hiringPartners/HiringPartners.jsx';
+import ExploreThePrograms from './components/exploreThePrograms/ExploreThePrograms.jsx';
 
+
+// internship_assingment
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Navbar/>
+    <Topview/>
+    <HiringPartners/>
+    <ExploreThePrograms/>
+  </>
   );
 }
 
---
+++ src/components/exploreThePrograms/ExploreThePrograms.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './exploreThePrograms.css'
+const ExploreThePrograms = () => {
+  return (
+    <div>
+       <div id="exploreThePrograms">
+              <div id="heading_exploreThePrograms">
+                     <h1>Explore The Programs</h1>
+              </div>
+       </div>
+      
+    </div>
+  )
+}
+
+export default ExploreThePrograms
